4cb3a0b90a9ffdaf6dcc49e6d4fd1ccb0ade7f51
EULERUR <Euler.Urs@kermi.de>
2022-10-24 11:40:20 +0200

    Add project files.

---
+++ EditorExample/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EditorExample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EditorExample/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace EditorExample;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Entry_Completed(object sender, EventArgs e)
+	{
+		label.Text = entry.Text;
+	}
+	
+	private void Entry_Completed_Unfocus(object sender, EventArgs e)
+	{
+		label1.Text = entry1.Text;
+		entry1.Unfocus();
+	}
+}
+
